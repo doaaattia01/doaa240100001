e1a7b16b5c6d3d9c710a8d4e9eafb1707ea94b61
doaaattia01 <doaa.attia01@gmail.com>
2025-12-11 13:12:50 +0200

    Refactor lateness calculation logic in Q18.cpp

--- dody practice/Q18.cpp
+++ dody practice/Q18.cpp
@@ -7,20 +7,18 @@ void solve() {
         return;
     }
 
-    // Calculate lateness in seconds (L)
     int lateness_seconds = P - S;
     int cups_owed = 0;
 
-    // Check lateness against the rules in descending order of penalty
     if (lateness_seconds <= 0) {
         cups_owed = 0;
-    } else if (lateness_seconds >= 1800) { // 30 minutes or more
+    } else if (lateness_seconds >= 1800) {  
         cups_owed = 4;
-    } else if (lateness_seconds >= 900) { // 15 minutes or more
+    } else if (lateness_seconds >= 900) {
         cups_owed = 3;
-    } else if (lateness_seconds >= 300) { // 5 minutes or more
+    } else if (lateness_seconds >= 300) { 
         cups_owed = 2;
-    } else { // At least 1 second late, but less than 5 minutes
+    } else { 
         cups_owed = 1;
     }
 
