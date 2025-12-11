ce8c3f4f1e680191dd03948f2a1f1aaffd64c157
doaaattia01 <doaa.attia01@gmail.com>
2025-12-11 13:13:14 +0200

    Clean up comments in solve function
    Removed irrelevant comments regarding input and explanation of the result.

--- dody practice/Q19.cpp
+++ dody practice/Q19.cpp
@@ -2,14 +2,10 @@
 
 void solve() {
     int N;
-    // Input N is read, but it is irrelevant to the solution.
     if (!(std::cin >> N)) {
         return;
     }
 
-    // The result is 0 because the required last 9 digits (987654321) 
-    // do not correspond to the last 9 digits of any perfect square.
-    // Specifically, 987654321 is not a quadratic residue modulo 27.
     
     std::cout << 0 << std::endl;
 }
